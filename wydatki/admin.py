7e446f69d6aab2c1a5be8565caabdd7e450b04ba
elangaar <elangaar@gmail.com>
2017-10-21 11:22:18 +0200

    added model registration for admin interface

--- wydatki/admin.py
+++ wydatki/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Reminder, IncomeSource, Income, Pocket, Place, Expense
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Reminder)
+admin.site.register(IncomeSource)
+admin.site.register(Income)
+admin.site.register(Pocket)
+admin.site.register(Place)
+admin.site.register(Expense)
